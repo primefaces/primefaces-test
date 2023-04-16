8a6ad89e2be43cc1b50e7bfa4a9c9aa269b101ee
Jasper de Vries <jasper.de.vries@ddnh.com>
2023-04-16 19:23:47 +0200

    Add list

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -6,6 +6,9 @@ import java.math.BigDecimal;
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import lombok.Data;
 
@@ -13,15 +16,22 @@ import lombok.Data;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String string;
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
-    
-    @PostConstruct  
+    private List<TestObject> list;
+
+    @PostConstruct
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+        list = new ArrayList<>(Arrays.asList(
+                new TestObject("Thriller", "Michael Jackson", 1982),
+                new TestObject("Back in Black", "AC/DC", 1980),
+                new TestObject("The Bodyguard", "Whitney Houston", 1992),
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
+        ));
     }
 
 }
